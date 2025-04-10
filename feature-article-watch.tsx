245e96ff80850a029468dbf86d9f81955abb88a1
LouiseStroehJp <louise.stroeh@jp.dk>
2025-04-10 11:01:11 +0200

    Add share icons

--- feature-article-watch.tsx
+++ feature-article-watch.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Button } from "./components/ui/button";
+import { Mail } from "lucide-react";
 
 export default function FeatureArticleWatch() {
   return (
@@ -28,13 +30,59 @@ export default function FeatureArticleWatch() {
                   />
                 </div>
                 <div className="flex flex-col">
-                  <span className="text-sm font-bold text-foreground">John Doe</span>
-                  <span className="text-sm text-muted-foreground">13. september 2024</span>
+                  <span className="text-sm font-bold text-foreground">John Doe Junior Hansen Jensen</span>
+                  <span className="text-sm text-muted-foreground">13. september 2024 kl. 16.31</span>
                 </div>
               </div>
             </div>
-            <div>
-              <span className="size-10 rounded-full text-muted-foreground block"></span>
+            <div className="flex flex-wrap items-center gap-2">
+              <div className="flex flex-wrap gap-2">
+                {/* LinkedIn */}
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="h-10 gap-2 rounded-full hover:border-transparent group/linkedin"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="h-4 w-4 fill-current"
+                    aria-hidden="true"
+                  >
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                  </svg>
+                  <span className="text-muted-foreground group-hover/linkedin:text-foreground hidden md:block">LinkedIn</span>
+                </Button>
+
+
+                {/* Microsoft Teams */}
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="h-10 gap-2 rounded-full hover:border-transparent group/teams"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="size-5 fill-current"
+                    aria-hidden="true"
+                  >
+                    <path d="M19.2 4.4C19.2 2.9 17.9 1.6 16.4 1.6C14.9 1.6 13.6 2.9 13.6 4.4C13.6 5.9 14.9 7.2 16.4 7.2C17.9 7.2 19.2 5.9 19.2 4.4ZM20 8H12.8C12.8 9.8 11.4 11.2 9.6 11.2C7.8 11.2 6.4 9.8 6.4 8H0V17.6C0 19.1 1.2 20.4 2.8 20.4H20.8C22.3 20.4 23.6 19.1 23.6 17.6V11.6C23.6 9.6 22 8 20 8ZM9.6 8C8.7 8 8 7.3 8 6.4C8 5.5 8.7 4.8 9.6 4.8C10.5 4.8 11.2 5.5 11.2 6.4C11.2 7.3 10.5 8 9.6 8ZM20.4 4.8H19.6V4H18.8V4.8H18V5.6H18.8V6.4H19.6V5.6H20.4V4.8Z" />
+                  </svg>
+                  <span className="text-muted-foreground group-hover/teams:text-foreground hidden md:block">Teams</span>
+                </Button>
+
+                {/* Email */}
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="h-10 gap-2 rounded-full hover:border-transparent group/email"
+                >
+                  <Mail className="size-5" />
+                  <span className="text-muted-foreground group-hover/email:text-foreground hidden md:block">Email</span>
+                </Button>
+
+              </div>
             </div>
           </div>
         </div>
@@ -50,12 +98,12 @@ export default function FeatureArticleWatch() {
               className="object-cover object-top"
               priority
             />
-            <p className="text-sm text-muted-foreground hidden 2xl:block 2xl:w-[200px] 2xl:absolute 2xl:bottom-0 2xl:right-[-216px]">
+            {/* <p className="text-sm text-muted-foreground hidden 2xl:block 2xl:w-[200px] 2xl:absolute 2xl:bottom-0 2xl:right-[-216px]">
               Ursula von der Leyen, President of the European Commission, speaks
               during a session of the European Parliament in Strasbourg, France.
-            </p>
+            </p> */}
           </div>
-          <p className="text-sm text-muted-foreground mt-2 2xl:hidden max-w-4xl mx-auto px-4">
+          <p className="text-sm text-muted-foreground mt-2 max-w-4xl mx-auto px-4">
             Ursula von der Leyen, President of the European Commission, speaks
             during a session of the European Parliament in Strasbourg, France.
           </p>
@@ -169,7 +217,7 @@ export default function FeatureArticleWatch() {
                   className="object-cover"
                 />
               </div>
-              <p className="text-xs lg:text-sm text-muted-foreground mt-2 text-center">
+              <p className="text-xs lg:text-sm text-muted-foreground mt-2">
                 Autonomous vehicles use AI to navigate complex environments
                 safely.
               </p>
